7b3a593107f80cda15536625fccabc1014b0aaa4
Phan Nguyen <nh314@outlook.com>
2017-12-26 09:27:53 +0700

    Ready for action

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ubuntu:16.04
+FROM ubuntu:14.04
 
 MAINTAINER phannh "nh314@outlook.com"
 
@@ -6,7 +6,9 @@ ADD . /usr/local
 
 EXPOSE 80
 
-RUN apt update -y
+RUN apt-get update -y
+
+RUN apt-get dist-upgrade -y
 
 RUN DEBIAN_FRONTEND=noninteractive apt install -yqq \
     apache2 \
@@ -21,7 +23,7 @@ RUN LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/apache2
 
 RUN apt-get update
 
-RUN apt install -yqq php5.6 \
+RUN apt-get install -yqq php5.6 \
 				php7.0 \
 				php7.1 \
 				php5.6-mbstring \
@@ -48,16 +50,8 @@ RUN a2enmod php5.6
 
 CMD ["echo", "hello"]
 
-RUN mkdir -p /var/lib/mysql && \
-	mkdir -p /var/run/mysqld && \
-	mkdir -p /var/log/mysql && \
-	chown -R mysql:mysql /var/lib/mysql && \
-	chown -R mysql:mysql /var/run/mysqld && \
-	chown -R mysql:mysql /var/log/mysql
+RUN install-composer.sh
 
-RUN sed -i -e "$ a [client]\n\n[mysql]\n\n[mysqld]"  /etc/mysql/my.cnf && \
-	sed -i -e "s/\(\[client\]\)/\1\ndefault-character-set = utf8/g" /etc/mysql/my.cnf && \
-	sed -i -e "s/\(\[mysql\]\)/\1\ndefault-character-set = utf8/g" /etc/mysql/my.cnf && \
-	sed -i -e "s/\(\[mysqld\]\)/\1\ninit_connect='SET NAMES utf8'\ncharacter-set-server = utf8\ncollation-server=utf8_unicode_ci\nbind-address = 0.0.0.0/g" /etc/mysql/my.cnf
+RUN install-node-js.sh
 
-RUN service apache2 start
+RUN install-phantom-js.sh
--- install-phantom-js.sh
+++ install-phantom-js.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
-PHANTOM_JS="phantomjs-1.9.8-linux-x86_64"
-if [[ $EUID -ne 0 ]]; then
-  echo "You must be a root user" 2>&1
-  exit 1
-else	
-	apt-get install -yqq build-essential chrpath libssl-dev libxft-dev 
-	apt-get install -yqq libfreetype6 libfreetype6-dev
-	apt-get install -yqq libfontconfig1 libfontconfig1-dev
-	apt-get install -yqq wget
-	cd ~
-	wget https://bitbucket.org/ariya/phantomjs/downloads/$PHANTOM_JS.tar.bz2
-	mv $PHANTOM_JS.tar.bz2 /usr/local/share/
-	cd /usr/local/share/
-	tar xvjf $PHANTOM_JS.tar.bz2
-	ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/share/phantomjs
-	ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/bin/phantomjs
-	ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/bin/phantomjs
-	rm -fr $PHANTOM_JS.tar.bz2
-fi
+apt-get install -yqq build-essential chrpath libssl-dev libxft-dev 
+apt-get install -yqq libfreetype6 libfreetype6-dev
+apt-get install -yqq libfontconfig1 libfontconfig1-dev
+apt-get install -yqq wget
+cd ~
+wget https://bitbucket.org/ariya/phantomjs/downloads/$PHANTOM_JS.tar.bz2
+mv $PHANTOM_JS.tar.bz2 /usr/local/share/
+cd /usr/local/share/
+tar xvjf $PHANTOM_JS.tar.bz2
+ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/share/phantomjs
+ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/bin/phantomjs
+ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/bin/phantomjs
+rm -fr $PHANTOM_JS.tar.bz2
